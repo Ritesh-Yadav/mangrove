dcf8d03da77d17fd2c1b6baa56f7f44ceefabc23
Rova Ratovo <rova.ratovo@gmail.com>
2011-08-30 14:18:31 +0300

    flatpages test

--- src/datawinners/settings.py
+++ src/datawinners/settings.py
@@ -66,7 +66,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+#ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -100,6 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 #    'datawinners.middleware.SSLMiddleware.SSLRedirect',
 )
 
@@ -126,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'django.contrib.flatpages',
     'datawinners.accountmanagement',
     'registration',
     'django.contrib.admin',
