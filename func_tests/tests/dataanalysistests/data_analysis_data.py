cc427db4f81bf68e4913fc21cf766b7854a21d6f
Ritesh Yadav <yadavr@thoughtworks.com>
2011-10-05 16:21:30 +0530

    Fixed functional tests: Ritesh

--- func_tests/tests/dataanalysistests/data_analysis_data.py
+++ func_tests/tests/dataanalysistests/data_analysis_data.py
@@ -1,4 +1,5 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from datetime import datetime
 
 PROJECT_NAME = "project_name"
 QUESTIONS = "questions"
@@ -7,6 +8,18 @@ DATE_RANGE = "date_range"
 CURRENT_MONTH = "current_month"
 LAST_MONTH = "last_month"
 YEAR_TO_DATE = "year_to_date"
+
+today = datetime.today()
+month = today.month - 1
+year = today.year
+if not(month):
+    month = 12
+    year = today.year - 1
+last_month_date = "12." + str(month) + "." + str(year)
+
+current_month_date = "01." + str(today.month) + "." + str(today.year)
+
+today_date = str(today.day) + "." + str(today.month) + "." + str(today.year)
 
 DEFAULT_DATA_FOR_QUESTIONNAIRE = {PROJECT_NAME: "clinic test project",
                       QUESTIONS: [u'What is your namé?',
@@ -22,27 +35,27 @@ DEFAULT_DATA_FOR_QUESTIONNAIRE = {PROJECT_NAME: "clinic test project",
                                      u'cid004 ànnita 30 07.03.2011 O- Dry cough,Neurological disorders 45.233,28.3324 --',
                                      u'cid005 Qamanda 47 12.03.2011 AB Dry cough,Memory loss 40.2,69.3123 --',
                                      u'cid007 Amanda 73 12.11.2010 AB Dry cough,Memory loss 40.2,69.3123 --',
-                                     u'cli10 Zorro 43 12.9.2011 O- Pneumonia,Memory loss 23.23452,-28.3456 --',
-                                     u'cli11 Aàntra 91 12.9.2011 O+ Dry cough,Neurological disorders -45.234,89.32345 --',
-                                     u'cli12 ànnita 45 12.9.2011 B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --',
-                                     u'cli13 Dmanda 89 01.10.2011 AB Pneumonia,Neurological disorders 40.2,69.3123 --']}
+                                     u'cli10 Zorro 43 %s O- Pneumonia,Memory loss 23.23452,-28.3456 --' % last_month_date,
+                                     u'cli11 Aàntra 91 %s O+ Dry cough,Neurological disorders -45.234,89.32345 --' % last_month_date,
+                                     u'cli12 ànnita 45 %s B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --' % last_month_date,
+                                     u'cli13 Dmanda 89 %s AB Pneumonia,Neurological disorders 40.2,69.3123 --' % current_month_date]}
 
 FILTER_BY_CURRENT_MONTH = {PROJECT_NAME: "clinic test project",
                            DATE_RANGE: CURRENT_MONTH,
-                           DATA_RECORDS: [u'cli13 Dmanda 89 01.10.2011 AB Pneumonia,Neurological disorders 40.2,69.3123 --',
-                                    u'cli14 Vamand 56 01.10.2011 O+ Rapid weight loss,Pneumonia,Neurological disorders 58.3452,115.3345 --',
-                                    u'cli15 M!lo 45 01.10.2011 AB Pneumonia,Rapid weight loss 19.672,92.33456 --',
-                                    u'cli16 K!llo 28 01.10.2011 O- Rapid weight loss,Neurological disorders 19.672,92.33456 --',
-                                    u'cli17 Catty 98 4.10.2011 O- Memory loss,Pneumonia,Neurological disorders 33.23452,-68.3456 --',
-                                    u'cli18 àntra 58 4.10.2011 O+ Rapid weight loss,Memory loss,Dry cough -45.234,169.32345 --',
-                                    u'cli9 Tinnita 27 4.10.2011 B+ Rapid weight loss,Pneumonia,Neurological disorders -78.233,-28.3324 --']}
+                           DATA_RECORDS: [u'cli13 Dmanda 89 %s AB Pneumonia,Neurological disorders 40.2,69.3123 --' % current_month_date,
+                                    u'cli14 Vamand 56 %s O+ Rapid weight loss,Pneumonia,Neurological disorders 58.3452,115.3345 --' % current_month_date,
+                                    u'cli15 M!lo 45 %s AB Pneumonia,Rapid weight loss 19.672,92.33456 --' % current_month_date,
+                                    u'cli16 K!llo 28 %s O- Rapid weight loss,Neurological disorders 19.672,92.33456 --' % current_month_date,
+                                    u'cli17 Catty 98 %s O- Memory loss,Pneumonia,Neurological disorders 33.23452,-68.3456 --' % today_date,
+                                    u'cli18 àntra 58 %s O+ Rapid weight loss,Memory loss,Dry cough -45.234,169.32345 --' % today_date,
+                                    u'cli9 Tinnita 27 %s B+ Rapid weight loss,Pneumonia,Neurological disorders -78.233,-28.3324 --' % today_date]}
 
 FILTER_BY_LAST_MONTH = {PROJECT_NAME: "clinic test project",
                            DATE_RANGE: LAST_MONTH,
-                           DATA_RECORDS: [u'cli10 Zorro 43 12.9.2011 O- Pneumonia,Memory loss 23.23452,-28.3456 --',
-                                    u'cli11 Aàntra 91 12.9.2011 O+ Dry cough,Neurological disorders -45.234,89.32345 --',
-                                    u'cli12 ànnita 45 12.9.2011 B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --',
-                                    u'cli9 Demelo 32 12.9.2011 AB Dry cough,Rapid weight loss 19.672,92.33456 --']}
+                           DATA_RECORDS: [u'cli10 Zorro 43 %s O- Pneumonia,Memory loss 23.23452,-28.3456 --' % last_month_date,
+                                    u'cli11 Aàntra 91 %s O+ Dry cough,Neurological disorders -45.234,89.32345 --' % last_month_date,
+                                    u'cli12 ànnita 45 %s B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --' % last_month_date,
+                                    u'cli9 Demelo 32 %s AB Dry cough,Rapid weight loss 19.672,92.33456 --' % last_month_date]}
 
 FILTER_BY_YEAR_TO_DATE = {PROJECT_NAME: "clinic test project",
                           DATE_RANGE: YEAR_TO_DATE,
@@ -52,13 +65,13 @@ FILTER_BY_YEAR_TO_DATE = {PROJECT_NAME: "clinic test project",
                                     u'cid003 Jimanda 86 12.03.2011 AB Dry cough,Memory loss 40.2,69.3123 --',
                                     u'cid004 ànnita 30 07.03.2011 O- Dry cough,Neurological disorders 45.233,28.3324 --',
                                     u'cid005 Qamanda 47 12.03.2011 AB Dry cough,Memory loss 40.2,69.3123 --',
-                                    u'cli10 Zorro 43 12.9.2011 O- Pneumonia,Memory loss 23.23452,-28.3456 --',
-                                    u'cli11 Aàntra 91 12.9.2011 O+ Dry cough,Neurological disorders -45.234,89.32345 --',
-                                    u'cli12 ànnita 45 12.9.2011 B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --',
-                                    u'cli13 Dmanda 89 01.10.2011 AB Pneumonia,Neurological disorders 40.2,69.3123 --',
-                                    u'cli14 Vamand 56 01.10.2011 O+ Rapid weight loss,Pneumonia,Neurological disorders 58.3452,115.3345 --',
-                                    u'cli15 M!lo 45 01.10.2011 AB Pneumonia,Rapid weight loss 19.672,92.33456 --',
-                                    u'cli16 K!llo 28 01.10.2011 O- Rapid weight loss,Neurological disorders 19.672,92.33456 --',
-                                    u'cli17 Catty 98 4.10.2011 O- Memory loss,Pneumonia,Neurological disorders 33.23452,-68.3456 --',
-                                    u'cli18 àntra 58 4.10.2011 O+ Rapid weight loss,Memory loss,Dry cough -45.234,169.32345 --',
-                                    u'cli9 Tinnita 27 4.10.2011 B+ Rapid weight loss,Pneumonia,Neurological disorders -78.233,-28.3324 --']}
+                                    u'cli10 Zorro 43 %s O- Pneumonia,Memory loss 23.23452,-28.3456 --' % last_month_date,
+                                    u'cli11 Aàntra 91 %s O+ Dry cough,Neurological disorders -45.234,89.32345 --' % last_month_date,
+                                    u'cli12 ànnita 45 %s B+ Pneumonia,Dry cough,Neurological disorders -78.233,-28.3324 --' % last_month_date,
+                                    u'cli13 Dmanda 89 %s AB Pneumonia,Neurological disorders 40.2,69.3123 --' % current_month_date,
+                                    u'cli14 Vamand 56 %s O+ Rapid weight loss,Pneumonia,Neurological disorders 58.3452,115.3345 --' % current_month_date,
+                                    u'cli15 M!lo 45 %s AB Pneumonia,Rapid weight loss 19.672,92.33456 --' % current_month_date,
+                                    u'cli16 K!llo 28 %s O- Rapid weight loss,Neurological disorders 19.672,92.33456 --' % current_month_date,
+                                    u'cli17 Catty 98 %s O- Memory loss,Pneumonia,Neurological disorders 33.23452,-68.3456 --' % today_date,
+                                    u'cli18 àntra 58 %s O+ Rapid weight loss,Memory loss,Dry cough -45.234,169.32345 --' % today_date,
+                                    u'cli9 Tinnita 27 %s B+ Rapid weight loss,Pneumonia,Neurological disorders -78.233,-28.3324 --' % today_date]}
