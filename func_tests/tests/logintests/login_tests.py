7e7a25085922ae6f95ec3b6385da52b52b33b66e
Wang Yan <yanwang@thoughtworks.com>
2011-10-18 13:12:48 +0800

    [Wang Yan & Kory -#733-]add function test for trial account expired

--- func_tests/tests/logintests/login_tests.py
+++ func_tests/tests/logintests/login_tests.py
@@ -2,9 +2,30 @@
 from nose.plugins.attrib import attr
 from framework.base_test import BaseTest
 from framework.utils.data_fetcher import from_, fetch_
+from pages import page
 from pages.loginpage.login_page import LoginPage
+from pages.page import Page
 from testdata.test_data import DATA_WINNER_LOGIN_PAGE
 from tests.logintests.login_data import *
+
+
+class ExpiredTrailPage(Page):
+    def __init__(self, driver):
+        Page.__init__(self, driver)
+
+    def get_error_message(self):
+     """
+     Function to fetch the error messages from error label of the login
+     page
+
+     Return error message
+     """
+     error_message = ""
+     locators = self.driver.find_elements_(ERROR_MESSAGE_LABEL)
+     if locators:
+         for locator in locators:
+             error_message = error_message + locator.text
+     return error_message.replace("\n", " ")
 
 
 class TestLoginPage(BaseTest):
@@ -12,7 +33,6 @@ class TestLoginPage(BaseTest):
     def test_login_with_valid_credentials(self):
         self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
         login_page = LoginPage(self.driver)
-
         dashboard_page = login_page.do_successful_login_with(VALID_CREDENTIALS)
         self.assertEqual(dashboard_page.welcome_message(),
                          fetch_(WELCOME_MESSAGE, from_(VALID_CREDENTIALS)))
@@ -72,3 +92,17 @@ class TestLoginPage(BaseTest):
         login_page = LoginPage(self.driver)
         register_page = login_page.navigate_to_registration_page()
         self.assertEqual(self.driver.get_title(), "Register")
+
+    @attr('functional_test')
+    def test_login_with_expired_trial_account(self):
+        self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
+
+        login_page = LoginPage(self.driver)
+        login_page.login_with(EXPIRED_TRAIL_ACCOUNT)
+
+        expired_trail_account_page = ExpiredTrailPage(self.driver)
+
+        self.assertEqual(expired_trail_account_page.get_error_message(),
+                         fetch_(ERROR_MESSAGE, from_(EXPIRED_TRAIL_ACCOUNT)))
+
+
