fdf0b8c5145e5126045c1ac3bb081f6941f15b9a
David Reed <dreed@thoughtworks.com>
2011-10-18 15:46:44 +0800

    Fixing mistake that resulted in dropping all CouchDB managers during runtests.sh (it should only drop the 'test' ones)

--- runtests.sh
+++ runtests.sh
@@ -37,7 +37,7 @@ cd src/datawinners
 if [ "${RECREATEDB}" != "N" -a "${RECREATEDB}" != "n" ]; then
     python manage.py syncdb --noinput
     python manage.py migrate
-    python manage.py recreatedb syncall
+    python manage.py recreatedb
 fi
 
 if [ "${TESTCHOICE}" != "ut" ]; then
