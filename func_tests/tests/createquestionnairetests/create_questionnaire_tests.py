32a17a77635239c25909eb851adcd3bfe3cbee28
Ritesh Yadav <Ritesh.Yadav@ThoughtWorks.com>
2011-11-04 16:59:45 +0530

    Fixing functional tests: Ritesh

--- func_tests/tests/createquestionnairetests/create_questionnaire_tests.py
+++ func_tests/tests/createquestionnairetests/create_questionnaire_tests.py
@@ -2,13 +2,13 @@
 from nose.plugins.attrib import attr
 from framework.base_test import BaseTest
 from framework.utils.data_fetcher import fetch_, from_
+from pages.createquestionnairepage.create_questionnaire_page import CreateQuestionnairePage
 from pages.dashboardpage.dashboard_page import DashboardPage
 from pages.loginpage.login_page import LoginPage
 from testdata.test_data import DATA_WINNER_LOGIN_PAGE
 from tests.logintests.login_data import VALID_CREDENTIALS
-from tests.createquestionnairetests.create_questionnaire_data import *
 from tests.createprojecttests.create_project_data import *
-from nose.plugins.skip import SkipTest
+from tests.createquestionnairetests.create_questionnaire_data import *
 
 
 class TestCreateQuestionnaire(BaseTest):
@@ -22,11 +22,8 @@ class TestCreateQuestionnaire(BaseTest):
         create_project_page = dashboard_page.navigate_to_create_project_page()
         #Navigating to Create Questionnaire Page by successfully creating a Project
         create_project_page.create_project_with(VALID_DATA2)
-        create_subject_questionnaire_page = create_project_page.save_and_create_project_successfully()
-        create_questionnaire_page = create_subject_questionnaire_page.save_questionnaire_successfully()
-        return create_questionnaire_page
+        return CreateQuestionnairePage(self.driver)
 
-    @SkipTest
     @attr('functional_test', 'smoke')
     def test_successful_questionnaire_creation(self):
         """
@@ -41,6 +38,6 @@ class TestCreateQuestionnaire(BaseTest):
             index += 1
         self.assertEquals(create_questionnaire_page.get_remaining_character_count(),
                           fetch_(CHARACTER_REMAINING, from_(QUESTIONNAIRE_DATA)))
-        create_questionnaire_page.save_questionnaire()
-        title = self.driver.wait_for_page_with_title(3, "Data Senders")
-        self.assertEquals("Data Senders", title)
+        create_questionnaire_page.save_and_create_project_successfully()
+        title = self.driver.wait_for_page_with_title(3, fetch_(PAGE_TITLE, from_(VALID_DATA2)))
+        self.assertEquals(fetch_(PAGE_TITLE, from_(VALID_DATA2)), title)
