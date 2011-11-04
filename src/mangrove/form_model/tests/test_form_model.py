e73eff090e31f021b8be8291b819cdec2255a0f8
roov7 <rova;ratovo@gmail.com>
2011-11-04 09:06:04 +0530

    Adding editable registration form

--- src/mangrove/form_model/tests/test_form_model.py
+++ src/mangrove/form_model/tests/test_form_model.py
@@ -6,7 +6,7 @@ from mangrove.datastore.documents import FormModelDocument
 from mangrove.datastore.entity_type import  define_type
 from mangrove.form_model.field import  TextField, IntegerField, SelectField
 from mangrove.errors.MangroveException import QuestionCodeAlreadyExistsException, EntityQuestionAlreadyExistsException, DataObjectAlreadyExists
-from mangrove.form_model.form_model import FormModel, create_default_reg_form_model, REGISTRATION_FORM_CODE
+from mangrove.form_model.form_model import FormModel, create_default_reg_form_model, REGISTRATION_FORM_CODE, create_reg_form_model
 from mangrove.datastore.datadict import DataDictType
 from mangrove.form_model.validation import NumericRangeConstraint, TextLengthConstraint, RegexConstraint
 
@@ -22,11 +22,17 @@ class TestFormModel(unittest.TestCase):
     def test_create_form_model(self):
         self.assertTrue(self.form_model__id)
 
-    def test_should_create_registration_form_model(self):
+    def test_should_create_default_registration_form_model(self):
         form = create_default_reg_form_model(self.dbm)
         self.assertEqual(7, len(form.fields))
         self.assertEqual(REGISTRATION_FORM_CODE, form.form_code)
-        self.assertEqual('string', form.fields[3].ddtype.primitive_type)
+        self.assertEqual('string', form.fields[2].ddtype.primitive_type)
+
+    def test_should_create_registration_form_model(self):
+        form = create_reg_form_model(self.dbm, "cli", "cli", ["clinic"])
+        self.assertEqual(4, len(form.fields))
+        self.assertEqual("cli", form.form_code)
+        self.assertEqual('string', form.fields[2].ddtype.primitive_type)
 
     def test_get_form_model(self):
         form = self.dbm.get(self.form_model__id, FormModel)
