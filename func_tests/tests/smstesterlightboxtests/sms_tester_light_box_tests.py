95f61869ee6ed2e322967a17849c4f605578e48a
Yu Fu <yufu@thoughtworks.com>
2011-11-07 13:39:46 +0800

    Refactoring functional tests.

--- func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
+++ func_tests/tests/smstesterlightboxtests/sms_tester_light_box_tests.py
@@ -14,37 +14,7 @@ USE_ORDERED_SMS_PARSER = False
 
 class BasePrepare(BaseTest):
     @nottest
-    def prerequisite_of_sms_test_light_box(self, project_data):
-        # doing successful login with valid credentials
-        self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
-        login_page = LoginPage(self.driver)
-        global_navigation = login_page.do_successful_login_with(VALID_CREDENTIALS)
-        # going on all project page
-        all_project_page = global_navigation.navigate_to_view_all_project_page()
-        project_overview_page = all_project_page.navigate_to_project_overview_page(
-            fetch_(PROJECT_NAME, from_(project_data)))
-        edit_project_page = project_overview_page.navigate_to_edit_project_page()
-        subject_questionnaire_page = edit_project_page.save_and_create_project_successfully()
-        questionnaire_page = subject_questionnaire_page.save_questionnaire_successfully()
-        datsender_questionnaire_page = questionnaire_page.save_questionnaire_successfully()
-        reminder_page = datsender_questionnaire_page.save_questionnnaire_successfully()
-        review_page = reminder_page.save_reminder_successfully()
-        return review_page.open_sms_tester_light_box()
-
-    @nottest
-    def prerequisites_of_sms_tester_light_box1(self):
-
-        project_data = PROJECT_DATA
-        return self.prerequisite_of_sms_test_light_box(project_data)
-
-    @nottest
-    def prerequisites_of_sms_tester_light_box3(self):
-
-        project_data = PROJECT_DATA_WITH_ACTIVITY_REPORT
-        return self.prerequisite_of_sms_test_light_box(project_data)
-
-    @nottest
-    def prerequisites_of_sms_tester_light_box2(self):
+    def prerequisites_of_sms_tester_light_box(self, project_to_open):
         # doing successful login with valid credentials
         self.driver.go_to(DATA_WINNER_LOGIN_PAGE)
         login_page = LoginPage(self.driver)
@@ -53,7 +23,7 @@ class BasePrepare(BaseTest):
         # going on all project page
         all_project_page = global_navigation.navigate_to_view_all_project_page()
         project_overview_page = all_project_page.navigate_to_project_overview_page(
-            fetch_(PROJECT_NAME, from_(PROJECT_DATA)))
+            fetch_(PROJECT_NAME, from_(project_to_open)))
         return project_overview_page.open_sms_tester_light_box()
 
 @skipIf(USE_ORDERED_SMS_PARSER, "USE_ORDERED_SMS_PARSER is set, only ordered sms can be used while this is the case")
@@ -64,7 +34,7 @@ class TestSMSTesterLightBox(BasePrepare):
         """
         Function to test the successful SMS submission
         """
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box2()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
         sms_tester_page.send_sms_with(VALID_DATA)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_DATA)))
 
@@ -73,19 +43,19 @@ class TestSMSTesterLightBox(BasePrepare):
         """
         Function to test the error message on the sms submission page for exceeding word limit for word type question
         """
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box2()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
         sms_tester_page.send_sms_with(EXCEED_NAME_LENGTH)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(EXCEED_NAME_LENGTH)))
 
     @attr('functional_test', 'smoke')
     def test_successful_sms_submission(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box2()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
         sms_tester_page.send_sms_with(VALID_DATA2)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_DATA2)))
 
     @attr('functional_test', 'smoke')
     def test_sms_submission_for_unicode(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box2()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
         sms_tester_page.send_sms_with(SMS_WITH_UNICODE)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(SMS_WITH_UNICODE)))
 
@@ -94,13 +64,13 @@ class TestOrderedSMSTesterLightBox(BasePrepare):
 
     @attr('functional_test')
     def test_successful_ordered_sms_submission(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box2()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA)
         sms_tester_page.send_sms_with(VALID_ORDERED_SMS_DATA)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_ORDERED_SMS_DATA)))
 
     @attr('functional_test')
     def test_sms_submission_for_project_with_activity_report(self):
-        sms_tester_page = self.prerequisites_of_sms_tester_light_box3()
+        sms_tester_page = self.prerequisites_of_sms_tester_light_box(PROJECT_DATA_WITH_ACTIVITY_REPORT)
         sms_tester_page.send_sms_with(VALID_ORDERED_SMS_DATA_WITH_ACTIVITY_REPORT)
         self.assertEqual(sms_tester_page.get_response_message(), fetch_(RESPONSE_MESSAGE, from_(VALID_ORDERED_SMS_DATA)))
 
