c9a2b9d5b58dec1aaf4d14fc47dac321ecd153ac
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-11-21 16:35:33 +0530

    Fixed couth fixtures

--- src/datawinners/main/initial_couch_fixtures.py
+++ src/datawinners/main/initial_couch_fixtures.py
@@ -1169,7 +1169,6 @@ def create_clinic_project_for_trial_account(CLINIC_ENTITY_TYPE, manager, trial_o
     )
 
     weekly_reminder_and_deadline = {
-            "reminders_enabled": "True",
             "deadline_week": "5",
             "deadline_type": "Same",
             "has_deadline": True,
