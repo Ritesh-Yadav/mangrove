f9e3748e50ad84fd31921f8cdd09523ac652a15f
roov7 <rova;ratovo@gmail.com>
2011-12-08 17:57:14 +0530

    Fixing create entity type

--- src/datawinners/media/javascript/entity/create_type.js
+++ src/datawinners/media/javascript/entity/create_type.js
@@ -21,25 +21,20 @@ $(document).ready(function() {
     $("#add_type").live("click", function() {
         $("#type_message").html("<span class='ajax_loader_small'></span>");
         var new_type = $("#id_entity_type_text").val().toLowerCase();
-        var default_form_model = $("#use-default-form").attr("checked");
-        $.post("/entity/type/create/", { entity_type_regex: new_type, default_form_model: default_form_model},
+        $.post("/entity/type/create/", { entity_type_regex: new_type },
                 function(response) {
                     var data = JSON.parse(response);
                     if (data.success) {
-                        if(default_form_model) {
-                            var options = $("#id_entity_type").attr('options');
-                            if (should_append(options, new_type)) {
-                                $("#id_entity_type").prepend($('<option></option>').val(new_type).html(new_type));
-                            }
-                            $('#id_entity_type').val(0);
-                            $('#id_entity_type').trigger('change');
-                            $("#add_subject_type").accordion({collapsible: true,autoHeight:false, active:2});
-                            $("#type_message").html('');
-                            $("#type_message").removeClass("message-box");
-                            $("#id_entity_type_text").val("");
-                        } else {
-                            window.location.replace('/entity/registration/' + new_type);
+                        var options = $("#id_entity_type").attr('options');
+                        if (should_append(options, new_type)) {
+                            $("#id_entity_type").prepend($('<option></option>').val(new_type).html(new_type));
                         }
+                        $('#id_entity_type').val(0);
+                        $('#id_entity_type').trigger('change');
+                        $("#add_subject_type").accordion({collapsible: true,autoHeight:false, active:2});
+                        $("#type_message").html('');
+                        $("#type_message").removeClass("message-box");
+                        $("#id_entity_type_text").val("");
                     }
                     else {
                         $("#type_message").html(data.message);
